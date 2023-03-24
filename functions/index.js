e824f30d5b09152dc5bb72b7b0803ce59c99218a
Utku ERDEN <31778258+Fisixus@users.noreply.github.com>
2023-03-24 00:48:52 -0400

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,9 @@
-const functions = require("firebase-functions");
+// The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access Firestore.
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
