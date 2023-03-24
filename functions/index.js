c9642d19eb59248833099cc148442cb4b4d74c11
Erden <erdenutku07@gmail.com>
2023-03-24 14:34:18 -0400

    All the necessary api calls are prepared.

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
 const functions = require('firebase-functions');
 
@@ -5,10 +7,88 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const testDBVersion = 'test';
+
+const getUserFunction = require('./getUser');
+const getVideoFunction = require('./getVideo');
+const getAllUsersFunction = require('./getAllUsers');
+const getCommentsOfVideoFunction = require('./getCommentsOfVideo');
+
+const updateCommentFunction = require('./updateComment');
+const updateVideoCommentsFunction = require('./updateVideoComments');
+const updateUserPasswordFunction = require('./updateUserPassword');
+
+const postUserFunction = require('./postUser');
+const postCommentFunction = require('./postComment');
+const postVideoFunction = require('./postVideo');
+
+const deleteVideoCommentFunction = require('./deleteVideoComment');
+
+exports.test_getUser = functions.https
+    .onCall((data) => 
+    {
+        return getUserFunction.handler(admin, testDBVersion, data);
+    });
+
+exports.test_getVideo = functions.https
+    .onCall((data) => 
+    {
+        return getVideoFunction.handler(admin, testDBVersion, data);
+    });
+
+exports.test_getAllUsers = functions.https
+    .onCall((data) => 
+    {
+        return getAllUsersFunction.handler(admin, testDBVersion, data);
+    });
+
+exports.test_getCommentsOfVideo = functions.https
+    .onCall((data) => 
+    {
+        return getCommentsOfVideoFunction.handler(admin, testDBVersion, data);
+    });
+
+exports.test_updateComment = functions.https
+    .onCall((data) => 
+    {
+        return updateCommentFunction.handler(admin, testDBVersion, data);
+    });
+
+exports.test_updateVideoComments = functions.https
+    .onCall((data) => 
+    {
+        return updateVideoCommentsFunction.handler(admin, testDBVersion, data);
+    });
+
+exports.test_updateUserPassword = functions.https
+    .onCall((data) => 
+    {
+        return updateUserPasswordFunction.handler(admin, testDBVersion, data);
+    });
+
+exports.test_postUser = functions.https
+    .onCall((data) => 
+    {
+        return postUserFunction.handler(admin, testDBVersion, data);
+    });
+
+exports.test_postVideo = functions.https
+    .onCall((data) => 
+    {
+        return postVideoFunction.handler(admin, testDBVersion, data);
+    });
+
+exports.test_postComment = functions.https
+    .onCall((data) => 
+    {
+        return postCommentFunction.handler(admin, testDBVersion, data);
+    });
+
+exports.test_deleteVideoComment = functions.https
+    .onCall((data) => 
+    {
+        return deleteVideoCommentFunction.handler(admin, testDBVersion, data);
+    });
+
+
+
